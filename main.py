1750f9ed844736d40562c16867c6381e934faa84
theodron <theodronorg@gmail.com>
2025-03-15 21:36:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bot import Bot
 
 Bot().run()
+invite_link = bot.exportChatInviteLink(chat_id="-100252317432")
+print(invite_link)
 print(f"Force Sub Channel: {FORCE_SUB_CHANNEL_2}")
 print(f"Bot permissions: {bot.getChat(FORCE_SUB_CHANNEL_2)}")
